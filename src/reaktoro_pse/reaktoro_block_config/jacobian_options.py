a7ad41e505d73ee94058eb442e13e619385f1c10
avdudchenko <33663878+avdudchenko@users.noreply.github.com>
2025-06-24 17:39:39 -0400

    Update jacobian_options.py

--- src/reaktoro_pse/reaktoro_block_config/jacobian_options.py
+++ src/reaktoro_pse/reaktoro_block_config/jacobian_options.py
@@ -125,7 +125,7 @@ class JacobianOptions:
         CONFIG.declare(
             "hessian_type",
             ConfigValue(
-                default=HessTypes.BFGS_damp,
+                default=HessTypes.LBFGS,
                 domain=IsInstance((str, HessTypes)),
                 description="Hessian type to use for reaktor gray box",
                 doc="""Hessian type to use, some might provide better stability
