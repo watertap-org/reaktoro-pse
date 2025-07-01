eedde5f96ecb2591ad97071dd1d3d998c8afc646
avdudchenko <33663878+avdudchenko@users.noreply.github.com>
2025-07-01 11:46:56 -0400

    trying new step size for tests

--- src/reaktoro_pse/reaktoro_block_config/jacobian_options.py
+++ src/reaktoro_pse/reaktoro_block_config/jacobian_options.py
@@ -43,7 +43,7 @@ class JacobianOptions:
         CONFIG.declare(
             "numerical_step",
             ConfigValue(
-                default=1e-4,
+                default=1e-5,
                 domain=float,
                 description="Defines the step to use for numerical descritiazaiton",
                 doc="""This will define how small of a step to use for numerical derivative propagation which takes
