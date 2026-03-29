9c7f02b4985bfcde3e7979a7c25027ec2b6f9007
avdudchenko <33663878+avdudchenko@users.noreply.github.com>
2026-03-29 01:05:35 -0400

    update scaling test

--- src/reaktoro_pse/reaktoro_block_config/jacobian_options.py
+++ src/reaktoro_pse/reaktoro_block_config/jacobian_options.py
@@ -92,7 +92,7 @@ class JacobianOptions:
         CONFIG.declare(
             "jacobian_scale_bounds",
             ConfigValue(
-                default=(1e-8, 1e2),
+                default=(1e-8, 1e6),
                 domain=IsInstance(tuple),
                 description="Defines lower and upper bounds for jacobian scaling factors",
                 doc="""
